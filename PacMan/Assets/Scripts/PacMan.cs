7badd82734025e5d0934c2714f68ecf0ce784d84
Dawnblood <13893983@student.uts.edu.au>
2020-09-21 11:47:09 +1000

    Tried doing PacMan Movement

--- PacMan/Assets/Scripts/PacMan.cs
+++ PacMan/Assets/Scripts/PacMan.cs
@@ -18,50 +18,88 @@ public class PacMan : MonoBehaviour
     private float TravelLeft;
     private float TravelUp;
 
+    [SerializeField]
+    private GameObject item;
+    private Tweener tweener;
+
     // Start is called before the first frame update
     void Start()
     {
-        startTime = Time.time;
+        /*startTime = Time.time;
 
         TravelRight = Vector3.Distance(TopLeft.position, TopRight.position);
         TravelDown = Vector3.Distance(TopRight.position, BottomRight.position);
         TravelLeft = Vector3.Distance(BottomRight.position, BottomLeft.position);
-        TravelUp = Vector3.Distance(BottomLeft.position, TopLeft.position);
+        TravelUp = Vector3.Distance(BottomLeft.position, TopLeft.position); */
+
+        tweener = gameObject.GetComponent<Tweener>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        StartCoroutine(MoveRight());
+        //StartCoroutine(MoveRight());
+
+        if (item.transform.position == TopLeft.position) {
+            tweener.AddTween(item.transform, item.transform.position, new Vector3(8.274508f,-0.93f,-120.4215f), 1.5f);
+        }
+        else if(item.transform.position == TopRight.position) {
+            tweener.AddTween(item.transform, item.transform.position, new Vector3(8.274508f,-6.05f,-120.4215f), 1.5f);
+        }
     }
 
     IEnumerator MoveRight() {
+
+        transform.position = new Vector3(1.92f, -0.93f, -120.4215f);
+        
         float distCovered1 =  (Time.time - startTime) * speed;
         float fractionOfDistance1 = distCovered1 / TravelRight;
 
         transform.position = Vector3.Lerp(TopLeft.position, TopRight.position, fractionOfDistance1);
 
+        Debug.Log("Moved Right");
         yield return new WaitForSeconds(5);
+
+        StartCoroutine(MoveDown());
+    }
+
+    IEnumerator MoveDown() {
+        transform.position = new Vector3(8.274508f, -0.93f, -120.4215f);
 
         float distCovered2 =  (Time.time - startTime) * speed;
         float fractionOfDistance2 = distCovered2 / TravelDown;
 
         transform.position = Vector3.Lerp(TopRight.position, BottomRight.position, fractionOfDistance2);
 
+        Debug.Log("Moved Down");
         yield return new WaitForSeconds(5);
+
+        StartCoroutine(MoveLeft());
+    }
+
+    IEnumerator MoveLeft() {
+        transform.position = new Vector3(8.274508f, -6.05f, -120.4215f);
 
         float distCovered3 =  (Time.time - startTime) * speed;
         float fractionOfDistance3 = distCovered3 / TravelLeft;
 
         transform.position = Vector3.Lerp(BottomRight.position, BottomLeft.position, fractionOfDistance3);
 
+        Debug.Log("Moved Left");
         yield return new WaitForSeconds(5);
+
+        StartCoroutine(MoveUp());
+    }
+
+    IEnumerator MoveUp() {
+        transform.position = new Vector3(1.92f, -6.05f, -120.4215f);
 
         float distCovered4 =  (Time.time - startTime) * speed;
         float fractionOfDistance4 = distCovered4 / TravelUp;
 
         transform.position = Vector3.Lerp(BottomLeft.position, TopLeft.position, fractionOfDistance4);
 
+        Debug.Log("Moved Up");
         yield return new WaitForSeconds(5);
     }
 }
---
+++ PacMan/Assets/Scripts/Tweener.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tweener : MonoBehaviour
+{
+    private Tween activeTween;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (activeTween != null) {
+
+            float timeFraction = (Time.time - activeTween.StartTime) / activeTween.Duration; 
+            activeTween.Target.position = Vector3.Lerp(activeTween.StartPos, activeTween.EndPos, timeFraction);
+
+            if (activeTween.Target.position == activeTween.EndPos) {
+                activeTween = null;
+            }
+        }
+    }
+
+    public void AddTween(Transform targetObject, Vector3 startPos, Vector3 endPos, float duration) {
+        if (activeTween == null) {
+            activeTween = new Tween(targetObject, startPos, endPos, Time.time, duration);
+        }
+    }
+}
